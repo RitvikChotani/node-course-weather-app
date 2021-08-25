88223d90cc8cf7f3e0bea1834b1972bc3401ff68
ritvik chotani <ritvikchotanino.1@gmail.com>
2021-08-26 04:09:15 +0530

    Added humidity info

--- utils/forecast.js
+++ utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (lat, long, callback) => {
 		} else {
 			callback(
 				undefined,
-				`${body.current.weather_descriptions[0]}, It is ${body.current.temperature}. It feels like ${body.current.feelslike}`,
+				`${body.current.weather_descriptions[0]}, It is ${body.current.temperature}. It feels like ${body.current.feelslike}. The humidity is ${body.current.humidity} %`,
 			);
 		}
 	});
